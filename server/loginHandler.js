e423278a58bedc73fad7135f4a81654fb67e39a3
Szabolcs Szabolcsi-Tóth <szabolcsit@hu.ibm.com>
2020-03-05 18:07:30 +0100

    use tito api to check booking references

--- server/loginHandler.js
+++ server/loginHandler.js
@@ -3,7 +3,6 @@ const uuid = require('uuid/v4')
 const moment = require('moment')
 const { promisify } = require('util')
 const fetch = require('isomorphic-unfetch')
-const checkUrl = process.env.TITO_CHECK_URL
 const store = require('./store')
 const tokenAuth = require('./token')
 
@@ -31,28 +30,42 @@ module.exports = async (req, res) => {
 		return
 	}
 
-	const check = await fetch(`${checkUrl}${ticketId}`)
+  try {
+    const check = await fetch(
+      `${process.env.TITO_API_URL}${ticketId}`,
+      {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${process.env.TITO_API_TOKEN}`
+        }
+      }
+    )
+    const result = await check.json();
 
-	if (check.status !== 200) {
-		res.sendStatus(403)
-		return
-	}
+    if (check.status !== 200 || result.tickets.length < 1) {
+      res.sendStatus(403)
+      return
+    }
 
-	const id = uuid()
-	const buf = await promisify(crypto.randomBytes)(256)
-	const secret = buf.toString('hex')
+    const id = uuid()
+    const buf = await promisify(crypto.randomBytes)(256)
+    const secret = buf.toString('hex')
 
-	const data = {
-		id,
-		ticketId,
-		secret,
-		workshop: '',
-		updated_at: moment().unix(),
-		created_at: moment().unix()
-	}
+    const data = {
+      id,
+      ticketId,
+      secret,
+      workshop: '',
+      updated_at: moment().unix(),
+      created_at: moment().unix()
+    }
 
-	store.set(ticketId, id)
-	store.hset('users', id, JSON.stringify(data))
+    store.set(ticketId, id)
+    store.hset('users', id, JSON.stringify(data))
 
-	sendToken(res, data)
+    sendToken(res, data)
+  } catch (e) {
+    console.error(e)
+    res.sendStatus(403)
+  }
 }
