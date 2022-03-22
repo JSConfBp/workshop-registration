eb88d50609219624f1011176d923f51743d84617
Szabolcs Szabolcsi-Toth <szabolcsit@hu.ibm.com>
2022-03-22 17:32:00 +0100

    Update workshops.js

--- workshops.js
+++ workshops.js
@@ -1,58 +1,16 @@
 module.exports = {
-	RISINGSTACK: {
-		closed: false,
-		created: "2019-05-17",
-		seats: 15, // 15
-		title: "High-Performance Microservices with GraphQL and Apollo",
-		short_description: "The attendees of this workshop are going to become more familiar with Apollo, both on the client and server side, and will learn how to implement both sides of a GraphQL server.",
+	XSTATE: {
+		closed: true,
+		created: "2022-03-22",
+		seats: 12,
+		title: "Diagram, Develop, Deploy: Build bulletproof apps collaboratively with statecharts",
+		short_description: "Using XState, the attendees of this workshop will learn how to model any piece of code using states, events, actions and services.",
 	},
-	TWILIO: {
-		closed: false,
-		created: "2019-05-17",
-		seats: 14, // 14
-		title: "Twilio Superclass",
-		short_description: "Superclass is hands-on training for developers to learn how to use all things communication using Twilio. Our engineers will guide you through the process of implementing messaging via WhatsApp and email. We will implement real-time features like chat and video, and glue all these things together in a single website using serverless functions.",
-	},
-	UNMOCKIO: {
-		closed: false,
-		created: "2019-05-17",
-		seats: 25, // 25
-		title: "Integration Driven Development",
-		short_description: "As the integration economy flourishes, authoring apps has never been easier. Tasks such as authentication, notifications, and search (to name a few) are now accessible via thousands of publicly available APIs. However, JavaScript is still a DIY language, with no native support for integrations and few established best practices. At the end of this workshop, you will be able to manage integrations as confidently as your own JavaScript code, helping you build resilient and robust apps around your core business logic.",
-	},
-	SUPERCHARGE: {
-		closed: false,
-		created: "2019-06-23",
-		seats: 15, // 15
-		title: "Let's jump on the JAMStack hype-train",
-		short_description: "Nowadays JAMStack is becoming more and more popular in JavaScript community. Join us and find out what does the JAMStack means while building an e-commerce site with Gatsby and a Headless CMS.",
-	},
-	MICROSOFT: {
-		closed: false,
-		created: "2019-07-07",
-		seats: 50,
-		title: "Serverless GraphQL with MS Azure",
-		short_description: "Learn how to Build a Serverless GraphQL API, using Node.js, Docker and Azure Serverless functions!",
-	},
-	BONOMI: {
-		closed: false,
-		created: "2019-08-12",
-		seats: 10,
-		title: "How to build an AI chatbot on Facebook Messenger",
-		short_description: "Now that the big hype around chatbots is over, it is time that the technology becomes useful in our everyday lives. Research shows that in certain countries, people already prefer talking to a chatbot rather than to a human agent. Come and join the lead developer of Bonomi who will help you build an application on the Messenger Platform that can respond to text input as intelligently as possible. Keywords: Typescript, Nodejs, Express, NLP, AI",
-	},
-	MITO: {
-		closed: false,
-		created: "2019-08-12",
+	NEWRELIC: {
+		closed: true,
+		created: "2022-03-22",
 		seats: 15,
-		title: "JavaScript Testing",
-		short_description: "During this crash course, you will learn the basics of testing in JavaScript: how to write testable code, unit tests and automated tests with the Cypress end to end testing framework.",
-	},
-	VACUUMLABS: {
-		closed: false,
-		created: "2019-08-25",
-		seats: 15,
-		title: "Reason: React and GraphQL",
-		short_description: "Join the ReasonML workshop powered by Vacuumlabs. We will build a real-world app using Reason language with the help of React and GraphQL.",
+		title: "Build Your Own Synthetics Monitoring",
+		short_description: "Build your own synthetics monitoring test suite and learn how to anticipate user behavior, both in all the expected ways and in all the unexpected ways, and build automated solutions to test for it.",
 	},
 }
