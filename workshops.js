8d72d97fe5beb955bef252bd0700ed9c55ae538d
Szabolcs Szabolcsi-Toth <szabolcsit@hu.ibm.com>
2022-04-09 21:11:01 +0200

    Update workshops.js

--- workshops.js
+++ workshops.js
@@ -2,29 +2,37 @@ module.exports = {
 	XSTATE: {
 		closed: true,
 		created: "2022-03-22",
-		seats: 12,
+		seats: 7,
 		title: "Diagram, Develop, Deploy: Build bulletproof apps collaboratively with statecharts",
 		short_description: "Using XState, the attendees of this workshop will learn how to model any piece of code using states, events, actions and services.",
 	},
 	NEWRELIC: {
 		closed: true,
 		created: "2022-03-22",
-		seats: 15,
+		seats: 7,
 		title: "Build Your Own Synthetics Monitoring",
 		short_description: "Build your own synthetics monitoring test suite and learn how to anticipate user behavior, both in all the expected ways and in all the unexpected ways, and build automated solutions to test for it.",
 	},
   EXADEL: {
     closed: true,
     created: "2022-03-25",
-    seats: 15,
+    seats: 7,
     title: "Mastering Node.js asynchronous flow",
     short_description: "Become a master of async flows in Node.js from event loop queues to working threads!",
   },
   HASURA: {
     closed: true,
     created: "2022-04-03",
-    seats: 15,
+    seats: 7,
     title: "Building realtime apps with GraphQL & Hasura",
     short_description: "tba",
   },
+  SUPERCHARGE: {
+    closed: true,
+    created: "2022-04-09",
+    seats: 10,
+    title: "Building realtime apps with GraphQL & Hasura",
+    short_description: "tba",
+  },  
+  
 }
