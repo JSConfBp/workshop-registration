da65019a94bd8434e000d7095d56290b7b2fe32a
Szabolcs Szabolcsi-Toth <szabolcsit@hu.ibm.com>
2022-05-02 21:05:38 +0200

    Update workshops.js

--- workshops.js
+++ workshops.js
@@ -1,31 +1,31 @@
 module.exports = {
 	XSTATE: {
 		created: "2022-03-22",
-		seats: 7,
+		seats: 15,
 		title: "Diagram, Develop, Deploy: Build bulletproof apps collaboratively with statecharts",
 		short_description: "Using XState, the attendees of this workshop will learn how to model any piece of code using states, events, actions and services.",
 	},
 	NEWRELIC: {
 		created: "2022-03-22",
-		seats: 7,
+		seats: 15,
 		title: "Build Your Own Synthetics Monitoring",
 		short_description: "Build your own synthetics monitoring test suite and learn how to anticipate user behavior, both in all the expected ways and in all the unexpected ways, and build automated solutions to test for it.",
 	},
   EXADEL: {
     created: "2022-03-25",
-    seats: 7,
+    seats: 12,
     title: "Mastering Node.js asynchronous flow",
     short_description: "Become a master of async flows in Node.js from event loop queues to working threads!",
   },
   HASURA: {
     created: "2022-04-03",
-    seats: 7,
+    seats: 15,
     title: "Building realtime apps with GraphQL & Hasura",
     short_description: "Learn how GraphQL provides simpler ergonomics for working with real-time data! Then, using Hasura, an open-source engine, instantly generate a subscription-ready API for your application!",
   },
   SUPERCHARGE: {
     created: "2022-04-09",
-    seats: 10,
+    seats: 20,
     title: "How to tame drones using face recognition?",
     short_description: "AI and IoT are not areas you should be afraid of! During our session, we’re going to learn how to convince drones to follow us everywhere, relying on face recognition.",
   },  
