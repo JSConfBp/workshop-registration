313d184405a15686ca9c5cedac2c14d53ba45900
Szabolcs Szabolcsi-Toth <hello@nec.is>
2024-04-27 22:14:08 +0200

    Update workshops.js

--- workshops.js
+++ workshops.js
@@ -2,16 +2,18 @@ module.exports = {
 	RISINGSTACK: {
 		created: "2024-04-22",
 		seats: 20,
+    coming_soon: true,
 		title: "AHA stack: Make web development easy again!",
 		short_description: "Meet the AHA stack, combining Astro, htmx, and Alpine.js to deliver rich, interactive sites in a surprisingly easy way.",
 	},
 	SUPERCHARGE: {
 		created: "2022-04-26",
 		seats: 25,
+    coming_soon: true,
 		title: "API-first approach with no-code stack for fullstack development",
 		short_description: "Learn the principles of the API-first approach, and how to implement API contracts using ChatGPT or Copilot and GitLab CI.",
 	},
-  CRABNEBULA: {
+  /* CRABNEBULA: {
     created: "2022-05-05",
     seats: 20,
     title: "Learn Tauri 2.0",
@@ -22,5 +24,5 @@ module.exports = {
     seats: 15,
     title: "Building with Svelte & SvelteKit",
     short_description: "TBA",
-  },
+  }, */
 }
